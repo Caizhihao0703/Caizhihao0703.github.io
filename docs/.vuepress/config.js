205225ad0e5ca23654526bc4f144a03fcb45c7c0
Caizhihao0703 <ccaius502@gmail.com>
2026-06-22 14:44:06 +0800

    Fix VuePress Pages build

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Cai Zhihao',
+  description: 'Personal homepage of Cai Zhihao',
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+  ],
+  themeConfig: {
+    search: false,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/Caizhihao0703' }
+    ]
+  }
+}
